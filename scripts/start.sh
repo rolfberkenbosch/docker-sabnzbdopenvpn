2d9d25e6971bda757fb4778864f61547fc25d833
Rolf Berkenbosch <rolf@berkenbosch.nl>
2019-05-09 21:14:37 +0200

    env settings

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,9 +14,7 @@ echo "STARTING SABNZBD with USER ${USER_NAME}"
 
 # ENV Variables missing - fix needed
 
-ENV LANG="en_US.UTF-8" \
-    LANGUAGE="en_US.UTF-8" \
-    PYTHONIOENCODING="utf-8"
-exec sudo -u abc /usr/bin/sabnzbdplus --config-file /config --server 0.0.0.0:8080 &
+ENV LANG="en_US.UTF-8" LANGUAGE="en_US.UTF-8" PYTHONIOENCODING="utf-8"
+exec sudo LANG="en_US.UTF-8" LANGUAGE="en_US.UTF-8" PYTHONIOENCODING="utf-8" -u abc /usr/bin/sabnzbdplus --config-file /config --server 0.0.0.0:8080 &
 
 echo "Startup script SABnzbd completed."
